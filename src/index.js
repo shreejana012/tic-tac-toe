6986374186af2dd6f5fe08281a559cbd1808c175
shreejana012 <shrijana460@gmail.com>
2017-12-05 11:56:09 +0545

    added helper function to determine winner

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ function Square(props){
       }
       handleClick(i){
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]){
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -29,13 +32,20 @@ function Square(props){
       }
     renderSquare(i) {
         // here value is props so used in Square component as props
-      return <Square value = {this.state.squares[i]}
+      return(<Square value = {this.state.squares[i]}
       onClick = { () => this.handleClick(i)}
-      />;
+      />
+      );
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner){
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
       return (
         <div>
@@ -82,4 +92,23 @@ function Square(props){
     <Game />,
     document.getElementById('root')
   );
-  
+
+  function calculateWinner(squares) {
+      const lines = [
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+          [0,3,6],
+          [1,4,7],
+          [2,5,8],
+          [0,4,8],
+          [2,4,6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && (squares[a] === squares[b]) && (squares[a] === squares[c])){
+              return squares[a];
+          }
+      }
+      return null;
+  }
